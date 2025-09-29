d8f37a0eea03214167de91bd4d73da8f456213ba
Anshika Agarwal <125692117+AnshikaAgarwal27@users.noreply.github.com>
2025-09-29 14:30:02 +0530

    Add files via upload

---
+++ app-automation-suite/src/main/java/com/appautomation/acres99/App.java
@@ -0,0 +1,10 @@
+package com.appautomation.acres99;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
